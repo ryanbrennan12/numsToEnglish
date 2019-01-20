a608d5b957113fab26e1beba2cd7d21069919fec
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-01-20 15:42:21 -0800

    fix: static page being served by server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+console.log(__dirname)
+
+app.use(express.static(__dirname + '/../dist'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// /Users/ryanbrennan/Desktop/Cover Letters/num2Eng/dist
